25890ae23d05b1e2517023a4d59123e8ca7517be
liangyue <shu@shu.moe>
2021-07-11 09:38:45 +0800

    update: add deploy script

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
 ./node_modules/.bin/hexo clean
-./node_modules/.bin/hexo build
+./node_modules/.bin/hexo generate
 cd public
 git init .
+git remote add origin https://ocian@github.com/ocian/blog.shu.moe.git
 git branch main
 git checkout main
 git add .
 git commit -m "update github pages"
-git push origin main:gh-pages
+git push origin main:gh-pages -f
